cfa415b222c25d4d125a225321f75ea951c94b3b
Bernhard Fisseni <bernhard.fisseni@mail.de>
2018-04-13 21:07:07 +0200

    Quote filter added

---
+++ protect-quote.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+"""
+Pandoc filter to protect U+2019 from smartness."""
+
+from pandocfilters import toJSONFilter, Str, RawInline
+import regex  # allow unicode character properties
+
+
+def protect_quote(key, value, format, meta):
+    """Protect U+2019 against https://github.com/jgm/pandoc/issues/4550"""
+    if format == "ms":
+        if key == 'Str':
+            if regex.search(r'[’]', value):
+                strs = regex.split(r'[’]', value)
+                ret = [Str(strs[0])]
+                for s in strs[1:]:
+                    ret += [RawInline("ms", "’"), Str(s)]
+                return ret
+
+
+if __name__ == "__main__":
+    toJSONFilter(protect_quote)
