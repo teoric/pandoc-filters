d327c2d7c20712a4fb62e41491a06ad6f8149bce
Bernhard Fisseni <bernhard.fisseni@mail.de>
2019-03-17 12:43:21 +0100

    image-list.lua: correction

--- image-list.lua
+++ image-list.lua
@@ -14,7 +14,7 @@
 --       Author: Bernhard Fisseni (teoric), <bernhard.fisseni@mail.de>
 --      Version: 0.5
 --      Created: 2019-03-17
--- Last Changed: 2019-03-17, 10:41:54 (CET)
+-- Last Changed: 2019-03-17, 12:42:59 (CET)
 --------------------------------------------------------------------------------
 --
 -- local inspect = require('inspect')
@@ -47,45 +47,54 @@ end
 
 -- convert SVG to PDF
 function convert_to_svg(im)
+  local img_orig = im.src
   if string.endswith(text.lower(im.src), ".pdf")
       or string.endswith(text.lower(im.src), ".eps") then
-    img_svg = string.gsub(im.src, "%.[^.]+$", ".svg")
+    im.src = string.gsub(im.src, "%.[^.]+$", ".svg")
     -- if not file_exists(img_svg) then
-    pandoc.pipe("pdf2svg", {im.src, img_svg}, "")
+    pandoc.pipe("pdf2svg", {img_orig, img.src}, "")
     -- end
   end
   return im
 end
 
--- convert SVG or PDF to EMF for inclusion into Word
+-- convert to EMF for inclusion into Word
 function convert_to_emf(im)
-  img_svg = im.src
+  local img_orig = im.src
   im = convert_to_svg(im)
-  im.src = string.gsub(im.src, "%.[^.]+$", ".emf")
-  if string.endswith(text.lower(im.src), ".pdf") then
-    pandoc.pipe("inkscape", {img_svg, "--export-emf", im.src}, "")
+  if string.endswith(text.lower(im.src), ".svg") then
+    im.src = string.gsub(im.src, "%.[^.]+$", ".emf")
+    pandoc.pipe("inkscape", {img_orig, "--export-emf", im.src}, "")
+  elseif (
+    not string.endswith(text.lower(im.src), ".emf") and not 
+    (
+      string.endswith(text.lower(im.src), ".png")
+      or
+      string.endswith(text.lower(im.src), ".jpg")
+      or
+      string.endswith(text.lower(im.src), ".jpeg")
+    )) then
+    -- let's try our best
+    im.src = string.gsub(im.src, "%.[^.]+$", ".emf")
+    pandoc.pipe("convert", {img_orig, im.src}, "")
   end
   return im
 end
 
-image_no = 0
+local image_no = 0
 
 return {
   {
     Image = function (im)
       image_no = image_no + 1
-      image_orig = im.src
+      local image_orig = im.src
       if FORMAT:find("html") or FORMAT:find("epub") then
         if string.endswith(text.lower(im.src), ".pdf") or
           string.endswith(text.lower(im.src), ".eps") then
             im = convert_to_svg(im)
           end
       elseif FORMAT == "docx" or FORMAT == "rtf" then
-        if string.endswith(text.lower(im.src), ".pdf") or
-          string.endswith(text.lower(im.src), ".eps") or
-          string.endswith(text.lower(im.src), ".svg") then
-          im = convert_to_emf(im)
-        end
+        im = convert_to_emf(im)
       end
       io.stderr:write(string.format("%-3d\t%s\t%s\n", image_no, im.src, image_orig))
       return im
