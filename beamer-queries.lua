90ff99e53404adef90a694fe232b9d402e6d92b4
Bernhard Fisseni <bernhard.fisseni@mail.de>
2019-07-25 20:02:42 +0200

    handling for ANNIS queries separated out

--- beamer-queries.lua
+++ beamer-queries.lua
@@ -4,7 +4,7 @@
 --
 --        Usage: pandoc --lua-filter=beamer-spans.lua
 --
---  Description: map spans in Pandoc markdown to beamer LaTeX commands
+--  Description: handle queries to corpus search engines
 --
 --       Author: Bernhard Fisseni (teoric), <bernhard.fisseni@mail.de>
 --      Version: 0.5
@@ -54,42 +54,18 @@ end
 
 
 return {{
-  Header = function(span)
-    if FORMAT == "beamer" then
-      if span.classes:includes("nofooter") then
-        span.content:extend({
-            pandoc.RawInline("beamer", "\\nofooter")
-        })
-        return span
-      end
-    end
-  end,
-
-  Span = function(span)
-    if FORMAT == "beamer" then
-      local start = nil
-      local finish = nil
-      if span.classes:includes("rechts") then
-        start = "\\rechts{"
-        finish = "}"
-      elseif span.classes:includes("rkomment") then
-        start = "\\rechts{\\emph{"
-        finish = "}}"
-      elseif span.classes:includes("emph") then
-        start = "\\oldemph{"
-        finish = "}"
-      elseif span.classes:includes("transl") then
-        start = "\\transl{"
-        finish = "}"
-      end
-      if start then
-        local ret = List:new({pandoc.RawInline("beamer", start)})
-        ret:extend(span.content)
-        ret:extend({pandoc.RawInline("beamer", finish)})
-        return {
-          pandoc.Span(ret, span.attr)
-        }
-      end
+  -- TODO: move to own file, does not depend on beamer.
+  Link = function(el)
+    if el.attributes["type"] == "ANNIS" then
+        local base = el.attributes["base"]
+        if not(string.endswith(base, "/")) then
+          base = base .. "/"
+        end
+        el.target =  base ..
+          "#_q=" .. base64(trim1(utils.stringify(el))) ..
+          "&_c=" .. base64(trim1(el.attributes["corpus"]))
+        -- io.stderr:write(el.target .. "\n")
+        return el
     end
   end
 }}
