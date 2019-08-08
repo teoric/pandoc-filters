daba8ba77ce5f0ed5edf6c12270f36454b1f3ee0
Bernhard Fisseni <bernhard.fisseni@mail.de>
2019-08-08 15:22:02 +0200

    full beamer-queries

--- beamer-queries.lua
+++ beamer-queries.lua
@@ -9,11 +9,11 @@
 --       Author: Bernhard Fisseni (teoric), <bernhard.fisseni@mail.de>
 --      Version: 0.5
 --      Created: 2018-03-30
--- Last Changed: 2019-07-26, 10:01:41 (CEST)
+-- Last Changed: 2019-08-08, 15:15:40 (CEST)
 --------------------------------------------------------------------------------
 --
 
--- local inspect = require('inspect')
+local inspect = require('inspect')
 text = require 'text'
 List = require 'pandoc.List'
 utils = require 'pandoc.utils'
@@ -26,14 +26,62 @@ require(debug.getinfo(1, "S").source:sub(2):match("(.*[\\/])") .. "utils")
 return {{
   -- TODO: move to own file, does not depend on beamer.
   Link = function(el)
-    if el.attributes["type"] == "ANNIS" then
+    if string.startswith(el.target, "file:") then
+      local file = string.gsub(el.target, "^file:/*", "")
+      if file_exists(file) then
+        io.stderr:write(string.format("will include %s\n", file))
+        return pandoc.RawInline(
+          "beamer",
+          string.format("\\textattachfile{%s}{%s}",
+            file,
+            utils.stringify(el)))
+      else
+        io.stderr:write("cannot attach '" .. file .. "' (does not exist).")
+      end
+    elseif el.attributes["type"] == "ANNIS" then
       local base = el.attributes["base"]
+      if base == nil then
+            io.stderr:write("base missing ".. utils.stringify(el) .."\n")
+      end
+      -- add slash to base if necessary
       if not(string.endswith(base, "/")) then
         base = base .. "/"
       end
+      local corpus = el.attributes["corpus"]
+      if corpus == nil then
+        io.stderr:write("corpus missing ".. utils.stringify(el) .."\n")
+      end
+      -- io.stderr:write(utils.stringify(el) .. "\n")
       el.target =  base ..
       "#_q=" .. base64(trim1(utils.stringify(el))) ..
-      "&_c=" .. base64(trim1(el.attributes["corpus"]))
+      "&_c=" .. base64(trim1(corpus))
+      -- io.stderr:write(el.target .. "\n")
+      return el
+    elseif el.attributes["type"] == "KorAP" then
+      local base = el.attributes["base"]
+      if base == nil then
+        io.stderr:write("base missing ".. utils.stringify(el) .."\n")
+      end
+      local lang = el.attributes["lang"]
+      if lang == nil then
+        io.stderr:write("lang missing ".. utils.stringify(el) .."\n")
+      end
+      -- io.stderr:write(utils.stringify(el) .. "\n")
+      el.target =  base ..
+      "?q=" .. urlencode(trim1(utils.stringify(el))) ..
+      "&ql=" .. urlencode(trim1(lang))
+      -- io.stderr:write(el.target .. "\n")
+      return el
+    elseif el.attributes["type"] == "RegExr" then
+      local base = "https://regexr.com/"
+      local text = el.attributes["text"]
+      if text == nil then
+        text = "Please enter some text to test the RegEx!"
+      end
+      -- io.stderr:write(utils.stringify(el) .. "\n")
+      el.target =  base ..
+      "?expression=" .. urlencode("/"..trim1(utils.stringify(el)).."/g") ..
+      "&engine=pcre&text=" .. urlencode(trim1(text))
       -- io.stderr:write(el.target .. "\n")
       return el
     elseif el.attributes["type"] == "DWDS" then
