cb834f21947c4ccecbf83dd898be798abdce6039
Bernhard Fisseni <bernhard.fisseni@mail.de>
2020-02-13 14:44:34 +0100

    add linebreak filter

---
+++ linebreaks.lua
@@ -0,0 +1,8 @@
+-- taken from https://stackoverflow.com/questions/28283008
+--- Transform a raw HTML element which contains only a `<br>`
+-- into a format-indepentent line break.
+function RawInline (el)
+  if el.format:match '^html' and el.text:match '%<br ?/?%>' then
+    return pandoc.LineBreak()
+  end
+end
